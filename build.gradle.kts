f3008d807e1c91fb9f70ffccc441062b7c07fdbf
Ryan Wong <ryan@rwmobi.com>
2024-06-01 20:04:44 +0100

    [chore] Gradle: Kotlin/Compose Compiler 2.0 migration

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,4 +9,5 @@ plugins {
     alias(libs.plugins.hilt.android.plugin) apply false
     alias(libs.plugins.devtools.ksp) apply false
     alias(libs.plugins.gradleKtlint) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
